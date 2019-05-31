7e30574f91530b317288d7b93ed3c23e1c92464f
haouzij <j@t.fr>
2019-05-31 16:27:25 +0200

    [MAIN] Updating main to collect and insert images + init structure dir

--- images-preprocessor/src/images_preprocessor/bin/main.py
+++ images-preprocessor/src/images_preprocessor/bin/main.py
@@ -2,19 +2,48 @@ import click
 from images_preprocessor.lib.logger.logger_manager import LoggerManager
 from images_preprocessor.lib.resources.resource_manager import ResourceManager
 from images_preprocessor.lib.images.image_manager import ImageManager
+from images_preprocessor.lib.mongo.mongo_manager import MongoManager
+from images_preprocessor.lib.common.common import init_directory_structure
 import os
+from logging import Logger
+from configparser import SectionProxy
+from pymongo.collection import Collection
+
+
+@click.command()
+@click.option('--env', help="Environment in which run the function", default="dev")
+def main_init_directory_structure(env: str):
+
+    resources: SectionProxy = ResourceManager(env).get_resources()
+    logger: Logger = LoggerManager("DIRECTORY", resources["LOG_DIR"], log_level="DEBUG").logger
+
+    init_directory_structure(resources, logger)
 
 
 @click.command()
 @click.option('--env', help="Environment in which run the function", default="dev")
 @click.option('--imgfile', help="Overwrite image file path", default="")
-def download_images(env: str, imgfile: str):
+def main_collect_insert_images(env: str, imgfile: str):
 
-    resources = ResourceManager(env).get_resources()
-    logger = LoggerManager("IMAGES", resources["LOG_DIR"], log_level="DEBUG").logger
+    resources: SectionProxy = ResourceManager(env).get_resources()
+    db_resources: SectionProxy = ResourceManager(env).get_resources(section="MONGODB")
 
-    image_file_path = imgfile if imgfile != "" else os.path.join(resources["DATA_DIR"], "urls.txt")
+    logger: Logger = LoggerManager("IMAGE", resources["LOG_DIR"], log_level="DEBUG").logger
+    db_logger: Logger = LoggerManager("MONGO", resources["LOG_DIR"], log_level="DEBUG").logger
+    main_logger: Logger = LoggerManager("MAIN", resources["LOG_DIR"], log_level="DEBUG").logger
+
+    image_file_path: str = imgfile if imgfile != "" else os.path.join(resources["DATA_DIR"], "urls.txt")
 
     image_manager = ImageManager(image_file_path, resources, logger)
-    image_manager.download_images()
+    mongo_manager = MongoManager(db_resources, db_logger)
+
+    mongo_manager.init_database()
+    collection: Collection = mongo_manager.create_collection("original_images")
+    documents: list = image_manager.collect_images()
+
+    state, result_ids = mongo_manager.insert_documents(collection, documents)
+
+    main_logger.info("Insertion state: {} with {} insertion".format(state, len(result_ids)))
+    main_logger.debug("Ids inserted: {}".format(result_ids))
+
 
