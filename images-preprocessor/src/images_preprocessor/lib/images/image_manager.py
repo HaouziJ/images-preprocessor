c0d1bb947dee41ebc20cd97ddc1ae6abaa206adc
haouzij <j@t.fr>
2019-05-31 16:29:35 +0200

    [IMAGE] New collect images function returning documents to insert

--- images-preprocessor/src/images_preprocessor/lib/images/image_manager.py
+++ images-preprocessor/src/images_preprocessor/lib/images/image_manager.py
@@ -6,16 +6,18 @@ import os
 from cv2 import imread, imwrite
 import hashlib
 from numpy import divide, ndarray
-from images_preprocessor.lib.resources.resource_manager import ResourceManager
+from configparser import SectionProxy
+from datetime import datetime
 
 
 class ImageManager:
 
-    def __init__(self, img_url_file: str, resources: ResourceManager, logger: Logger = None):
+    def __init__(self, img_url_file: str, resources: SectionProxy, logger: Logger = None):
         self.__img_url_file = img_url_file
         self.__resources = resources
         self.__logger = logger
         self.__img_url_list: list = []
+        self.__documents: list = []
 
     @property
     def img_url_file(self):
@@ -36,6 +38,10 @@ class ImageManager:
     @logger.setter
     def logger(self, logger):
         self.__logger = logger
+
+    @property
+    def documents(self):
+        return self.__documents
 
     @retry(HTTPError, tries=2, delay=3, backoff=1, logger=None)
     def __url_open(self, request):
@@ -66,10 +72,15 @@ class ImageManager:
                 with open(image_path, 'wb') as output:
                     self.logger.debug("Writing image to {}...".format(image_path))
                     output.write(image)
+                return image
+            else:
+                return "error"
         except ValueError:
             self.logger.error("Bad image link: {}".format(image_link))
+            return "error"
         except Exception as error:
             self.logger.error(error)
+            return "error"
 
     def __read_image(self, image_file: str):
         try:
@@ -112,9 +123,12 @@ class ImageManager:
             self.logger.info("Parsing urls from file {}...".format(self.img_url_file))
             self.img_url_list: list = file.read().split("\n")
 
-    def download_images(self):
+    def collect_images(self):
         self.__get_images_link()
         url_length: int = len(self.img_url_list)
         self.logger.info("Starting downloading for {} images...".format(url_length))
         for url, index in zip(self.img_url_list, range(url_length)):
-            self.__download_image(url, index)
+            image = self.__download_image(url, index)
+            self.logger.debug({"url": url, "timestamp": datetime.now().strftime("%Y%m%d%H%M")})
+            self.documents.append({"url": url, "image": image, "timestamp": datetime.now().strftime("%Y%m%d%H%M")})
+        return self.documents
