8c9d720410e1db730692ba9851c185bbe697b1d4
haouzij <j@t.fr>
2019-06-01 03:01:31 +0200

    [MONGO] Adding new functions to handle collections

--- images-preprocessor/src/images_preprocessor/lib/mongo/mongo_manager.py
+++ images-preprocessor/src/images_preprocessor/lib/mongo/mongo_manager.py
@@ -4,6 +4,8 @@ from pymongo.database import Database
 from pymongo.collection import Collection
 from configparser import SectionProxy
 
+from pymongo.errors import DuplicateKeyError
+
 
 class MongoManager:
     def __init__(self, resources: SectionProxy, logger: Logger = None):
@@ -28,6 +30,12 @@ class MongoManager:
     def logger(self, logger: Logger):
         self.__logger = logger
 
+    @staticmethod
+    def __clear_collection(collection: Collection):
+        collection.drop_indexes()
+        item_deleted = collection.delete_many({})
+        return item_deleted.deleted_count
+
     def init_database(self):
         self.__client = MongoClient(self.resources["HOST"], int(self.resources["PORT"]))
         self.logger.debug(self.__client.server_info())
@@ -35,18 +43,34 @@ class MongoManager:
         self.__database: Database = self.__client[self.resources["NAME"]]
         return self.__database.name
 
-    def create_collection(self, collection_name: str):
-        self.logger.info("Creating new collection {}...".format(collection_name))
+    def get_collection(self, collection_name: str, index_collection: str = None, overwrite: bool = False):
+        self.logger.info("Getting collection {}...".format(collection_name))
         collection = self.__database[collection_name]
+        if overwrite:
+            self.logger.debug("Deleting {} items...".format(self.__clear_collection(collection=collection)))
+        if index_collection:
+            self.logger.info("Creating index on {}...".format(index_collection))
+            collection.create_index(index_collection, unique=True)
         self.logger.debug(self.__database.list_collection_names())
         return collection
 
-    def insert_documents(self, collection: Collection, documents: list):
+    def insert_many_documents(self, collection: Collection, documents: list):
         try:
             result = collection.insert_many(documents)
-            return "done", result.inserted_ids
+            return "success", result.inserted_ids
         except Exception as error:
             self.logger.exception(error)
             return "error", []
+
+    def insert_one_document(self, collection: Collection, document: dict):
+        try:
+            result = collection.insert_one(document)
+            return "success", result.inserted_id
+        except DuplicateKeyError as error:
+            self.logger.warning("Key already exists {}".format(error))
+            return "error", ""
+        except Exception as error:
+            self.logger.exception(error)
+            return "error", ""
 
 
