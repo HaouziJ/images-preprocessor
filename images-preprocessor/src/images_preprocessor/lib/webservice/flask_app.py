3a480179e5aea038ba4cb2ae8a9ce0d48adff9d6
haouzij <j@t.fr>
2019-06-02 16:57:36 +0200

    [FLASK] Setting up flask manager and flask app for images and monitoring

--- images-preprocessor/src/images_preprocessor/lib/webservice/flask_app.py
+++ images-preprocessor/src/images_preprocessor/lib/webservice/flask_app.py
@@ -1,26 +1,30 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, send_file
 from flask_pymongo import PyMongo
 import os
 from configparser import SectionProxy
 from images_preprocessor.lib.resources.resource_manager import ResourceManager
 from images_preprocessor.lib.logger.logger_manager import LoggerManager
+from images_preprocessor.lib.webservice.flask_manager import FlaskManager
 from logging import Logger
 import pandas
+import io
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 resources: SectionProxy = ResourceManager(env=os.environ["ENV"]).get_resources(section="MONGODB")
 logger: Logger = LoggerManager(logger_name="IMAGE", log_folder=resources["LOG_DIR"], log_level="DEBUG").logger
 
+
 app = Flask(__name__)
-app.config["MONGO_URI"] = "{}://{}:{}/{}".format(resources["DRIVER"],
-                                                 resources["HOST"],
-                                                 resources["PORT"],
-                                                 resources["NAME"])
 
-mongo = PyMongo(app)
+flask_instance = FlaskManager(app=app, resources=resources)
+flask_instance.init_flask_app()
+
+mongo = PyMongo(flask_instance.app)
 
 
-@app.route("/")
+@app.route('/home')
 def index():
     results = mongo.db.target_images.find()
     return render_template("index.html", data=results)
@@ -28,40 +32,28 @@ def index():
 
 @app.route("/image/<string:md5>")
 def image(md5):
+    # 3afd392d481cb31625f86d87e38b7cbc
     results = mongo.db.target_images.find_one_or_404({"md5": md5})
-    return render_template("image.html", md5=results["md5"])
+    return send_file(io.BytesIO(results["image"]), mimetype='image/jpg')
 
 
+@app.route('/transfo_per_minute.png')
+def transformation_per_minute():
+
+    transformation = mongo.db.monitoring.find()
+
+    df_transformation = pandas.DataFrame(list(transformation))
+
+    sns.catplot(y="timestamp", hue="state", kind="count", palette="pastel", edgecolor=".6", data=df_transformation)
+
+    output = io.BytesIO()
+    plt.savefig(output, format='png')
+    output.seek(0)
+
+    return send_file(output,  mimetype='image/png')
+
+
+@app.route("/")
 @app.route("/monitoring")
 def monitoring():
-    total = mongo.db.monitoring.find({}).count()
-
-    md5_success = mongo.db.monitoring.find({"md5_state": "success"}).count()
-    md5_error = mongo.db.monitoring.find({"md5_state": "error"}).count()
-
-    gray_success = mongo.db.monitoring.find({"gray_state": "success"}).count()
-    gray_error = mongo.db.monitoring.find({"gray_state": "error"}).count()
-
-    md5_count = mongo.db.monitoring.aggregate([
-        {'$group':
-            {'_id': {'timestamp': "$timestamp", "md5_state": "$md5_state"}, 'count': {'$sum': 1}}
-         }
-    ])
-
-    gray_count = mongo.db.monitoring.aggregate([
-        {'$group':
-            {'_id': {'timestamp': "$timestamp", "gray_state": "$gray_state"}, 'count': {'$sum': 1}}
-         }
-    ])
-
-    stats = mongo.db.monitoring.find()
-
-    df = pandas.DataFrame(list(stats)).groupby(["timestamp", "md5_state", "gray_state"]).count()
-
-    return render_template("monitoring.html", md5_success=md5_success,
-                           md5_error=md5_error,
-                           gray_success=gray_success,
-                           gray_error=gray_error,
-                           total=total,
-                           md5_count=md5_count,
-                           gray_count=gray_count, df=df)
+    return render_template("monitoring.html")
